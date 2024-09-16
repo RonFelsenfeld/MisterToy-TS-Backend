6941ececdc3c7f097229706092cbdd3813f707ca
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-09-16 12:37:05 +0300

    Update .gitignore

--- dist/models/db.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDBConfig = createDBConfig;
-function createDBConfig(dbURL) {
-    return { dbURL, dbName: process.env.DB_NAME };
-}
